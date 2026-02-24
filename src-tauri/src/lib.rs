b6b96dd9fec3581ed8cc12849c82b6ff36514071
Louise <102354122+Ravish-Vishwakarma@users.noreply.github.com>
2026-02-24 18:37:24 +0530

    fuzzy search

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -28,6 +28,7 @@ fn scan_memes(folder_path: String) -> Vec<String> {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_clipboard_manager::init())
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![greet, scan_memes])
         .run(tauri::generate_context!())
--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,80 @@
 import "./App.css";
 import { Button } from "./components/ui/button";
-import { Folder, FolderPlus } from "lucide-react";
+import { Folder, FolderPlus, RefreshCcw } from "lucide-react";
 import { invoke } from "@tauri-apps/api/core";
 import { Input } from "./components/ui/input";
-
+import Fuse from "fuse.js";
+import { writeText } from "@tauri-apps/plugin-clipboard-manager";
+import { useState } from "react";
+import { convertFileSrc } from "@tauri-apps/api/core";
+import { useMemo } from "react";
 function App() {
+  const [memes, setMemes] = useState<
+    { path: string; name: string }[]
+  >([]);
+  const [search, setSearch] = useState("");
 
   const handelClick = async () => {
     try {
-      const result = await invoke("scan_memes", {
+      const result = await invoke<string[]>("scan_memes", {
         folderPath: "E:\\louisedrive\\Social Assets\\Memes + Sound Effects\\Indian Humour",
       });
 
-      console.log(result);
-      // setMemes(result as string[]);
+      const formatted = result.map((path) => ({
+        path,
+        name: path.split("\\").pop() || "",
+      }));
+
+      setMemes(formatted);
     } catch (error) {
       console.error("Error scanning memes:", error);
     }
   };
 
+  const handelcheck = () => {
+    console.log(filteredMemes)
+  }
+
+  const fuse = useMemo(() => {
+    return new Fuse(memes, {
+      keys: ["name"],
+      threshold: 0.4,
+    });
+  }, [memes]);
+
+  const filteredMemes = search
+    ? fuse.search(search).map((r) => r.item)
+    : memes;
   return (
     <main className="container">
       <div>
 
-        <div className="flex justify-center">
-          <Input id="input-demo-api-key" type="password" placeholder="meme name.." />
+        <div className="flex flex-row justify-evenly p-10">
+          <Input value={search} onChange={(e) => setSearch(e.target.value)} id="input-demo-api-key" placeholder="meme name.." />
 
           <Button variant={"outline"} size={"icon"} aria-label="Add Meme Folder">
             <FolderPlus></FolderPlus>
           </Button>
-          <Button variant={"outline"} size={"icon"} aria-label="Open Meme Folder">
+          <Button onClick={handelcheck} variant={"outline"} size={"icon"} aria-label="Open Meme Folder">
             <Folder></Folder>
           </Button>
+          <Button onClick={handelClick} variant={"outline"} size={"icon"} aria-label="Open Meme Folder">
+            <RefreshCcw ></RefreshCcw >
+          </Button>
+        </div>
+        <div className="mt-6 grid grid-cols-3 gap-4 p-6">
+          {filteredMemes.map((meme, index) => (
+            <div key={index} className="border rounded-xl p-2">
+              <img
+                src={`${convertFileSrc(meme.path)}`}
+                alt="meme"
+                className="w-full h-40 object-cover rounded-lg"
+              />
+              <p className="text-sm mt-2 truncate">
+                {meme.name}
+              </p>
+            </div>
+          ))}
         </div>
       </div>
     </main>
